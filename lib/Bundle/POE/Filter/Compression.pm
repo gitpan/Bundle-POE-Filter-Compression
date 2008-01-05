1222b84c93dac0a2252971f4190466b5fbc02fab
Chris Williams <chris@bingosnet.co.uk>
2008-01-05 13:02:42 +0000

    Import of BINGOS/Bundle-POE-Filter-Compression-1.00 from CPAN.
    gitpan-cpan-distribution: Bundle-POE-Filter-Compression
    gitpan-cpan-version:      1.00
    gitpan-cpan-path:         BINGOS/Bundle-POE-Filter-Compression-1.00.tar.gz
    gitpan-cpan-author:       BINGOS
    gitpan-cpan-maturity:     released

---
+++ lib/Bundle/POE/Filter/Compression.pm
@@ -0,0 +1,42 @@
+package Bundle::POE::Filter::Compression;
+
+use strict;
+use vars qw($VERSION);
+
+$VERSION = '1.00';
+
+1;
+
+__END__
+
+=head1 NAME
+
+Bundle::POE::Filter::Compression - Bundle to install all compression related POE Filters.
+
+=head1 SYNOPSIS
+
+    perl -MCPANPLUS -e 'install Bundle::POE::Filter::Compression'
+
+=head1 DESCRIPTION
+
+Bundle::POE::Filter::Compression - Bundle to install all compression related POE Filters.
+
+=head1 CONTENTS
+
+POE 0.9999
+
+POE::Filter::Bzip2 1.54
+
+POE::Filter::LZF 1.64
+
+POE::Filter::LZO 1.64
+
+POE::Filter::LZW 1.64
+
+POE::Filter::Zlib 1.93
+
+=head1 AUTHOR
+
+Chris Williams <chris@bingosnet.co.uk>
+
+=cut
